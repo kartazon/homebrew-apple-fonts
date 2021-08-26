4cc82dc4bed945ece0d10fba26696909413938f2
kartazon <44315828+kartazon@users.noreply.github.com>
2021-08-26 09:59:34 +0300

    Update font-new-york.rb

--- Casks/font-new-york.rb
+++ Casks/font-new-york.rb
@@ -2,7 +2,7 @@ cask 'font-new-york' do
     version :latest
     sha256 :no_check
 
-    url 'https://developer.apple.com/design/downloads/NY-Font.dmg'
+    url 'https://devimages-cdn.apple.com/design/resources/download/NY.dmg'
     name 'New York'
     homepage 'https://developer.apple.com/fonts/'
 
